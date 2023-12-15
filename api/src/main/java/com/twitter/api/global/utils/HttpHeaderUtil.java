a79f518bd318c2ffcbe08c535758c1ec5ecf70a4
FaberJoo <sjdk0805@gmail.com>
2023-12-15 23:40:37 +0900

    feat: clientIP 수집 및 헤더 로직을 위한 HttpHeaderUtil 추가 #1

---
+++ api/src/main/java/com/twitter/api/global/utils/HttpHeaderUtil.java
@@ -0,0 +1,19 @@
+package com.twitter.api.global.utils;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class HttpHeaderUtil {
+    public static String getClientIP(HttpServletRequest request) {
+        String clientIP = request.getHeader("X-Forwarded-For");
+
+        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+            clientIP = request.getHeader("X-Real-IP");
+        }
+
+        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+            clientIP = request.getRemoteAddr();
+        }
+
+        return clientIP;
+    }
+}
