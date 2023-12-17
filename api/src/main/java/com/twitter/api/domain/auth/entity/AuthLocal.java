77c36209ace98b761b9b7530614c67d252ce97f6
FaberJoo <sjdk0805@gmail.com>
2023-12-17 23:21:15 +0900

    feat: 로컬 인증 엔티티 추가 #2

---
+++ api/src/main/java/com/twitter/api/domain/auth/entity/AuthLocal.java
@@ -0,0 +1,34 @@
+package com.twitter.api.domain.auth.entity;
+
+import com.twitter.api.domain.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Getter
+public class AuthLocal{
+    @Id
+    @GeneratedValue(generator = "uuid")
+    private String id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 75)
+    private String password;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+}
