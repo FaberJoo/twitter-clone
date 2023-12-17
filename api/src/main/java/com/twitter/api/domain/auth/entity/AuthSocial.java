10dc7591fe5dd26b4d015873441c9617eae33052
FaberJoo <sjdk0805@gmail.com>
2023-12-17 23:21:46 +0900

    feat: 소셜 인증 엔티티 추가 #2

---
+++ api/src/main/java/com/twitter/api/domain/auth/entity/AuthSocial.java
@@ -0,0 +1,34 @@
+package com.twitter.api.domain.auth.entity;
+
+import com.twitter.api.domain.BaseTimeEntity;
+import com.twitter.api.domain.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class AuthSocial{
+    @Id
+    @GeneratedValue(generator = "uuid")
+    private String id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    @Column(nullable = false, length = 50, unique = true, updatable = false)
+    private String socialId;
+
+    @Column(nullable = false, length = 100, updatable = false)
+    private String email;
+}
